2fc75ac6db702e2cffe02d37c0f60119a62fccb2
vkumar85 <vijay.sangha@gmail.com>
2022-06-21 14:16:07 -0400

    delibrate break

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -32,7 +32,7 @@ public class MessageProcessor {
 				                          .withName(incomingMessage.get("vehicle"))
 				                          .withLat(new BigDecimal(incomingMessage.get("lat")))
 				                          .withLng(new BigDecimal(incomingMessage.get("long")))
-				                          .withTimestamp(convertedDatestamp)
+				                          .withSpeed(47.5)
 				                          .build();
 				                          
 		data.updatePosition(newReport);
